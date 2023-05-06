12f36f867124ef14c7853a4e0616980c33458687
mahdizaremz66 <mahdizare.mz66@gmail.com>
2023-05-07 01:58:45 +0430

    u140202170100

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
 import FooterComponent from "./components/ui/Footer.components";
 import NavbarComponent from "./components/ui/Navbar.component";
+import { Routes ,Route} from "react-router-dom";
+import HomePage from "./pages/Home.page";
+import FAQPage from "./pages/FAQ.page";
+import AboutUsPage from "./pages/AboutUs.page";
+import ContactUsPage from "./pages/ContactUs.page";
+
 
 function App() {
   return (
@@ -9,7 +15,13 @@ function App() {
 
       {/*Wapper*/}
       <div className="wapper">
-        <h2>Wapperr</h2>
+        <Routes>
+          <Route path="/" element= {<HomePage />} />
+          <Route path="/faq" element= {<FAQPage />} />
+          <Route path="/about-us" element= {<AboutUsPage />} />
+          <Route path="/contact-us" element= {<ContactUsPage />} />
+
+        </Routes>
 
       </div>
 
---
+++ src/pages/AboutUs.page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const AboutUsPage = () => {
+      return (
+        <div>
+            <h1>AboutUsPage</h1>
+            <h1>AboutUsPage</h1>
+            <h1>AboutUsPage</h1>
+        </div>
+    )
+  }
+
+export default  AboutUsPage;
